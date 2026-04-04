12724c51c17e795ddde1033760a1825ebf015101
olorunyomiemma65-cmyk <olorunyomiemma65@gmail.com>
2026-04-04 20:22:16 +0100

    updated admin.js

--- admin-firebase.js
+++ admin-firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 // ── EMAILJS CONFIG ──
 const EMAILJS_PUBLIC_KEY       = "mgXvrQKIT2yNaWdH3";
 const EMAILJS_SERVICE_ID       = "service_fpdlbxb";
-const EMAILJS_CANCEL_TEMPLATE  = "template_cance";    // ← create this template in EmailJS
+const EMAILJS_CANCEL_TEMPLATE  = "template_d0ajzh7";    // ← create this template in EmailJS
 const EMAILJS_CONFIRM_TEMPLATE = "template_d0ajzh7";   // ← create this template in EmailJS
 
 // ======= ADD ALL ADMIN EMAILS HERE =======
